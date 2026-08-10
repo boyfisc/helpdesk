b2431b0f9312ab4de5b37b5c1dcb66fb5840f4b0
boyfisc <sembeneimpot@gmail.com>
2026-08-10 17:36:17 +0000

    bug fix

--- patch-server.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.ts', 'utf-8');
-content = content.replace("const { createServer: createViteServer } = await import('vite');", "const viteModule = 'vite';\n    const { createServer: createViteServer } = await import(viteModule /* @vite-ignore */);");
-fs.writeFileSync('server.ts', content);
